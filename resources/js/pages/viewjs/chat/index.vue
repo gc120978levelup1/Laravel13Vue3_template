33a70b4415ddf192f27c280b3e9fac8e5f08c58f
Garry Cacho <gc120978levelup1@gmail.com>
2026-04-12 23:59:03 +0800

    giannah-cacho-grade1

--- resources/js/pages/viewjs/chat/index.vue
+++ resources/js/pages/viewjs/chat/index.vue
@@ -176,7 +176,7 @@ const msg_div = ref();
 
             <div ref="msg_div" class="flex flex-col lg:flex-row gap-6">
                 <div class="w-full flex flex-col gap-2 mb-0">
-                    <Label class="font-bold" for="address">
+                    <Label class="font-bold" for="message">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                             class="lucide lucide-mail-plus-icon lucide-mail-plus">
@@ -185,8 +185,8 @@ const msg_div = ref();
                             <path d="M19 16v6" />
                             <path d="M16 19h6" />
                         </svg>Message</Label>
-                    <Input type="text" placeholder="ex. McArthur Park, Beverly Hills, Samar, Philipines" name="address"
-                        v-model="form.address" />
+                    <Input type="text" placeholder="ex. McArthur Park, Beverly Hills, Samar, Philipines" name="message"
+                        v-model="form.message" />
                 </div>
             </div>
 
