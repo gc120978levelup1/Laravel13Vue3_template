8349dea158ce55ea99ec4f0b115c5ed83d2f846f
Garry Cacho <gc120978levelup1@gmail.com>
2026-04-12 23:40:10 +0800

    gianna-bella-chatbot

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,30 +10,19 @@ class ChatController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('viewjs/chat/index',['response' => $request["message"]]); // Renders the Vue component
+        return Inertia::render('viewjs/chat/index', ['response' => $request["message"]]); // Renders the Vue component
     }
 
     public function sendMessage(Request $request)
     {
-
         $response = Http::withToken(env('OPENAI_API_KEY'))
             ->post('https://api.openai.com/v1/responses', [
                 'model' => 'gpt-5.3',
                 'input' => $request["message"],
             ]);
 
-        //return response()->json([
-        //    'reply' => $response['output'][0]['content'][0]['text'] ?? 'No response'
-        //]);
-
-
-        //return redirect()->route(
-        //    'chat.index',[
-        //        'response' => $request["message"]//$response['output'][0]['content'][0]['text'] ?? 'No response'
-        //    ]
-        //);
-        return response()->json([
-            'response' => $response
+        return Inertia::render('viewjs/chat/index', [
+            'response' => $response['output'][0]['content'][0]['text'] ?? 'No response'
         ]);
     }
 
