23caba7d6efd29e68073662115dfa9ae46e71df6
Garry Cacho <gc120978levelup1@gmail.com>
2026-04-13 04:46:23 +0800

    gia-cacho-kii

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,20 +10,24 @@ class ChatController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('viewjs/chat/index', ['response' => $request["message"]]); // Renders the Vue component
+        return Inertia::render('viewjs/chat/index', [
+            'response' => $request["response"]
+        ]); // Renders the Vue component
     }
 
     public function sendMessage(Request $request)
     {
-        $response = Http::withToken(env('OPENAI_API_KEY'))
-            ->post('https://api.openai.com/v1/responses', [
-                'model' => 'gpt-5.3',
-                'input' => $request["message"],
-            ]);
+        $response = Http::withToken(env('OPENAI_API_KEY'))->post('https://api.openai.com/v1/responses', [
+            'model' => 'gpt-4o-mini', //'gpt-5.3',
+            'input' => $request["message"],
+        ]);
 
-        return Inertia::render('viewjs/chat/index', [
+        return redirect()->route('chat.index', [
             'response' => $response['output'][0]['content'][0]['text'] ?? 'No response'
         ]);
+        //return Inertia::render('viewjs/chat/index', [
+        //    'response' => $response['output'][0]['content'][0]['text'] ?? 'No response'
+        //]);
     }
 
 }
